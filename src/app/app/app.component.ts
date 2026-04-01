4a47672ee587edac492cfec60e6a0a5ce8fcc4d5
Hyvom Rahmouni <hyvomrahmouni00@gmail.com>
2026-04-01 02:22:41 +0100

    navbar added & styled

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatbotWidgetComponent } from '../shared/chatbot-widget/chatbot-widget.component';
+import { NavbarComponent } from '../navbar/navbar.component'; // ← ADD THIS LINE
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, ChatbotWidgetComponent]
+  imports: [RouterOutlet, ChatbotWidgetComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'student-profile';
