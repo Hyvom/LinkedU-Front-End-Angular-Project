8eb43b734801ccdd953a5efd26026606c6d0ef51
mohameddhiasakkar <mohameddhiasakkar@gmail.com>
2026-04-01 12:40:13 +0100

    new home and pics style

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatbotWidgetComponent } from '../shared/chatbot-widget/chatbot-widget.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component'; // ← ADD THIS LINE
 
 @Component({
@@ -8,7 +9,7 @@ import { NavbarComponent } from '../navbar/navbar.component'; // ← ADD THIS LI
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, ChatbotWidgetComponent, NavbarComponent]
+  imports: [RouterOutlet, ChatbotWidgetComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'student-profile';
